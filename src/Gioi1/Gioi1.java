9a10f75c9aa97591345c147055864d47a8d77780
NamNTHE176286 <145975790+NamNT2942003@users.noreply.github.com>
2026-01-03 16:57:48 +0700

    Initial commit

---
+++ src/Gioi1/Gioi1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Gioi1 {
+    public  static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rawAmount = 1000000;
+
+        do {
+            try {
+                System.out.println("Nhập vào số tiền cần rút: ");
+                int amount = Integer.parseInt(scanner.nextLine());
+                if (amount > rawAmount) {
+                    System.out.println("Lỗi : Số tiền rút vượt quá số dư! ");
+
+                }else if(amount <= 0){
+                    System.out.println("Vui lòng nhập vào 1 số hợp lệ");
+                }
+                else if (rawAmount - amount <= 50000) {
+                    System.out.println("Lỗi: Tài khoản phải duy trì số dư tối thiểu 50.000 đồng!");
+                }
+                else {
+                    rawAmount = rawAmount - amount;
+                    System.out.println("Số tiền đã rút: " + amount);
+                    System.out.println("Số tiền còn lại: "+ rawAmount);
+                }
+            }catch (Exception e){
+                System.out.println("Vui lòng nhập vào 1 số hợp lệ");
+            }
+        }while(true);
+    }
+}
